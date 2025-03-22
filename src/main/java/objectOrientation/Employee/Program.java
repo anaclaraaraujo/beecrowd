b4aa95e1a448ac775ed2f6d2b9194313e3828b21
anaclaraaraujo <helloanaclaraaraujo@gmail.com>
2025-03-21 22:21:53 -0300

    feat: introductory exercises to object orientation

---
+++ src/main/java/objectOrientation/Employee/Program.java
@@ -0,0 +1,30 @@
+package objectOrientation.Employee;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Employee employee = new Employee();
+
+
+        System.out.print("Name: ");
+        employee.name = sc.nextLine();
+        System.out.print("Gross salary: ");
+        employee.grossSalary = sc.nextDouble();
+        System.out.print("Tax: ");
+        employee.tax = sc.nextDouble();
+
+        System.out.println();
+
+        System.out.println("Employee: " + employee);
+
+        System.out.print("Which percentage to increase salary? ");
+        double percentage = sc.nextDouble();
+        employee.increaseSalary(percentage);
+
+        System.out.println("Updated data: " + employee);
+
+    }
+}
